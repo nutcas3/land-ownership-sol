87ded5bd183ef01706c68a83d1f7197fb18f76f2
nutcas3 <mauricee423@gmail.com>
2024-05-27 11:08:20 +0300

    Feat : Introduce Land struct and management functionality
    Land struct: Defines a schema for storing land details including location, owner, area, and state.
    Land storage: Implements a mapping (lands) to store Land structs using a unique property ID as the key.
    Land counter: Tracks the total number of lands added (landCount).
    addLand function: Allows adding new land properties with specified details. It automatically generates a unique property ID, sets initial values for non-provided fields (price, registered time, employee ID, scheduled date, rejected reason, state), and stores the Land object in the mapping.

--- contracts/Properties.sol
+++ contracts/Properties.sol
@@ -1,20 +1,64 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-
 contract Property {
-    
-
     // Created   : Represents, inintal state, when property details are added by user
     // Scheduled : Represents, when correponding surveyer scheduled for verification
     // Rejected  : Represents, property is rejected by surveyer
     // Verified  : Represents, property is verified.
-    enum StateOfProperty { 
-        Created, 
-        Scheduled, 
-        Verified, 
+    enum StateOfProperty {
+        Created,
+        Scheduled,
+        Verified,
         Rejected,
         OnSale,
         Bought
-        }
+    }
+
+    struct Land {
+        uint256 propertyId;
+        uint256 locationId;
+        uint256 revenueDepartmentId;
+        uint256 surveyNumber;
+        address owner;
+        uint256 area;
+        uint256 price;
+        uint256 registeredTime;
+        address employeeId;
+        string scheduledDate;
+        string rejectedReason;
+        StateOfProperty state;
+    }
+
+    mapping(uint256 => Land) public lands;
+
+    //generate propert id
+    uint256 private landCount;
+
+    function addLand(
+        uint256 _locationId,
+        uint256 _revenueDepartmentId,
+        uint256 _surveyNumber,
+        address _owner,
+        uint256 _area
+    ) public returns (uint256) {
+        landCount++;
+
+        lands[landCount]  = Land({
+            propertyId: landCount,
+            locationId: _locationId,
+            revenueDepartmentId: _revenueDepartmentId,
+            surveyNumber: _surveyNumber,
+            owner: _owner,
+            area: _area,
+            price:0,
+            registeredTime: block.timestamp,
+            employeeId: address(0),
+            scheduledDate: "",
+            rejectedReason: "",
+            state: StateOfProperty.Created
+        });
+        // return propertyId
+        return landCount;
+    }
 }
